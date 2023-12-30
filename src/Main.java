49583bdcc6928601ee0094ef7e4eb591e8529df6
Arhizmei <arhizmei@gmail.com>
2023-12-30 17:09:21 +0200

    hello world 3

--- src/Main.java
+++ src/Main.java
@@ -5,3 +5,7 @@ console.log("Hello world");
 // Завдання: https://shpp.gitbook.io/zero/tutorials/commands/hello-world#chastina-2
 
 console.log("hello\nworld");
+
+// Завдання: https://shpp.gitbook.io/zero/tutorials/commands/hello-world#chastina-3
+
+console.log(12892323 + 454665768);
