fadec5c426d9e2679b4cc98300f8e1315cafe8f8
Arhizmei <arhizmei@gmail.com>
2023-12-30 17:15:10 +0200

    swap 2

--- src/Main.java
+++ src/Main.java
@@ -9,3 +9,18 @@ console.log("hello\nworld");
 // Завдання: https://shpp.gitbook.io/zero/tutorials/commands/hello-world#chastina-3
 
 console.log(12892323 + 454665768);
+
+// Завдання: https://shpp.gitbook.io/zero/tutorials/commands/swap#chastina-2
+let first = 5;
+let second = 10;
+
+console.log(first);
+console.log(second);
+let third = 0;
+
+third = second;
+second = first;
+first = third;
+
+console.log(first);
+console.log(second);
